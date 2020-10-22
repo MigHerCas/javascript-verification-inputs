3563e58673fccfd755ecf95ccb13add79ad70d1d
Miguel Hernanz <migherdcas@gmail.com>
2020-10-22 22:26:38 +0200

    Automated form submit implemented + backspace event handling.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const form = document.querySelector('[name="verify"]');
-const inputs = form.querySelectorAll('.inputs input');
+const inputs = Array.from(form.querySelectorAll('.inputs input'));
 
 function handleInput(e) {
   // check for data that was inputtted and if there is a next input, focus it
@@ -13,26 +13,37 @@ function handlePaste(e) {
   const paste = e.clipboardData.getData('text');
   // loop over each input, and populate with the index of that string
   inputs.forEach((input, i) => {
-    console.log(input);
     input.value = paste[i] || '';
   });
+
+  if (paste.length >= inputs.length) form.submit();
 }
 
 inputs[0].addEventListener('paste', handlePaste);
 form.addEventListener('input', handleInput);
 
-// 123456
-// 785817
 
 // 1. select the text when the next input is focued
 function handleFocus(e) {
   const input = e.target;
-  console.log(input);
   if (input.value) e.target.select();
 }
 
-[].forEach.call(inputs, (input) => input.addEventListener('focus', handleFocus));
-
+inputs.map((input) => input.addEventListener('focus', handleFocus));
 
 // 2. Auto submit the form if all fields are filled after a paste
+inputs[inputs.length - 1].addEventListener('input', () => {
+  !inputs.some((input) => input.value === '') && form.submit() && console.log('Form submitted! 🚀');
+});
+
 // 3. support for backspacing from 1 input to another
+function handleBackspace(e) {
+  const input = e.target;
+
+  // Backspace key code is 8
+  if (e.keyCode === 8 && input.previousElementSibling) {
+    input.previousElementSibling.focus();
+  }
+}
+
+inputs.map((input) => input.addEventListener('keydown', handleBackspace));
